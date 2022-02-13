66d24791326963bdbf5981852d6bff219fa47330
kweiwen <kweiwen.tseng@gmail.com>
2022-02-13 17:08:46 +0800

    hello world

--- src/HelloWorldOfADSP_21489.cpp
+++ src/HelloWorldOfADSP_21489.cpp
@@ -5,21 +5,17 @@
 #include <sys/platform.h>
 #include "adi_initialize.h"
 #include "HelloWorldOfADSP_21489.h"
+#include <iostream>
 
-/** 
- * If you want to use command program arguments, then place them in the following string. 
- */
 char __argv_string[] = "";
 
 int main(int argc, char *argv[])
 {
-	/**
-	 * Initialize managed drivers and/or services that have been added to 
-	 * the project.
-	 * @return zero on success 
-	 */
 	adi_initComponents();
 	
+	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Hello, World!" << std::endl;
+
 	/* Begin adding your custom code here */
 
 	return 0;
