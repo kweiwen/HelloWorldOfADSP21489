66fca9357e235c3741683eabd21824309b95b4c6
kweiwen <kweiwen.tseng@gmail.com>
2022-02-13 19:46:59 +0800

    update

--- hello-world/src/hello-world.cpp
+++ hello-world/src/hello-world.cpp
@@ -1,27 +1,14 @@
-/*****************************************************************************
- * hello-world.cpp
- *****************************************************************************/
-
 #include <sys/platform.h>
+#include "init_PLL_SDRAM.h"
 #include "adi_initialize.h"
-#include "hello-world.h"
 #include <iostream>
 
-/** 
- * If you want to use command program arguments, then place them in the following string. 
- */
 char __argv_string[] = "";
 
 int main(int argc, char *argv[])
 {
-	/**
-	 * Initialize managed drivers and/or services that have been added to 
-	 * the project.
-	 * @return zero on success 
-	 */
 	adi_initComponents();
 	
-	/* Begin adding your custom code here */
 
 	return 0;
 }
---
+++ hello-world/src/init_PLL_SDRAM.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void Init_PLL(void);
+void Init_SDRAM(void);
+void Init_SRAM(void);
+void Init_AMI(void);
+void Delay(const int n);
