9f840d1a3a922b3114d65ae93b21f4fb4aba0dcd
kweiwen <kweiwen.tseng@gmail.com>
2022-02-13 17:06:27 +0800

    update

---
+++ src/HelloWorldOfADSP_21489.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * HelloWorldOfADSP_21489.h
+ *****************************************************************************/
+
+#ifndef __HELLOWORLDOFADSP_21489_H__
+#define __HELLOWORLDOFADSP_21489_H__
+
+/* Add your custom header content here */
+
+#endif /* __HELLOWORLDOFADSP_21489_H__ */
