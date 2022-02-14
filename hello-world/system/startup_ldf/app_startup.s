97c41709fa28c1a2c334467535ca69d015f5341a
kweiwen <kweiwen.tseng@gmail.com>
2022-02-14 09:21:40 +0800

    update ldf

--- hello-world/system/startup_ldf/app_startup.s
+++ hello-world/system/startup_ldf/app_startup.s
@@ -1,5 +1,5 @@
 /*
-** ADSP-21489 startup code generated on Feb 13, 2022 at 19:25:32.
+** ADSP-21489 startup code generated on Feb 14, 2022 at 09:21:06.
 */
 /*
 ** Copyright (C) 2000-2021 Analog Devices Inc., All Rights Reserved.
