7de69e6af07e650fede4eec656adadc62a8299e3
DelayNoMore <251382196@qq.com>
2022-02-19 16:19:07 +0800

    串口在ADI-21489 EZ BOARD上運作正常,每次接收並返回8個字節(刪除hello_world.cpp,將代碼移動到main.cpp)

--- hello-world/src/mian.cpp
+++ hello-world/src/mian.cpp
@@ -3,68 +3,73 @@
 #include "init_PLL_SDRAM.h"
 #include "initSRU.h"
 #include <sru.h>
-#include <services/int/adi_int.h>  			/* Interrupt Handler API header. */
-#include <sru21489.h>						/* Get definition of SRU() */
-#include <stdlib.h>							/* Get definition of NULL */
+#include <services/int/adi_int.h>
+#include "Goble_Define.h"
+#include "hw_connect.h"
 #include "UART0_isr.h"
-#include "ADDS_21489_EzKit.h"
-//manchiu git hub test 1
-//manchiu git hub test 2
-char TEST[] =
-{
-		"\f"
-		"TEST!"
-		"\n"
-};
+#include "initSPORT01_I2S_mode.h"
+#include "SPORT1_isr.h"
+#include "blockProcess_audio.h"
 
 void Delay_Cycles(unsigned int delayCount);
+void Codec_Reset(void);
 
+void initUART(void);
+void Init_UART_Baud(int baud,int Len,char Party,int Stop);
+
+
+
+char welcomemessage[] = {"\fHello! \n\r"
+                  "This program echoes back whatever you type in...\n\r"};
+
+
+
+extern volatile int timer_isr_count;
+extern volatile int commandReady;
+extern char uart_buffer[];
 int main(void)
 {
-	Init_PLL();
+
+ 	Init_PLL();
 	Init_SDRAM();
 	initDAI();
 
-   	SRU2(UART0_TX_O, DPI_PB07_I);	/* UART TX signal is connected to DPI pin 9 */
-	SRU2(HIGH, DPI_PBEN07_I);
-
-	SRU2(DPI_PB08_O, UART0_RX_I);	/* connect the pin buffer output signal to the UART0 RX */
-	SRU2(LOW, DPI_PB08_I);
-	SRU2(LOW, DPI_PBEN08_I);			/* disable DPI pin10 as input */
-
-	SRU2(LOW, DPI_PB14_I);
-	SRU2(HIGH, DPI_PBEN14_I);
-
 	/* init UART and install interrupt */
 	initUART();
-    adi_int_InstallHandler(ADI_CID_P14I, UART0ISR, NULL, true);
+	adi_int_InstallHandler(ADI_CID_P14I, UART0ISR, NULL, true);
 	adi_int_InstallHandler(ADI_CID_TMZLI, TimerISR, (void *)&timer_isr_count, true);
 
+	initSPORT();
+	adi_int_InstallHandler(ADI_CID_P3I, TalkThroughISR, 0, true);
+
 	std::cout << "Hello, World!" << std::endl;
+	DBG(welcomemessage);
+
+
 
 	int flag = 0;
-	int buffer_cntr = 0;
-
-
 	while(1)
 	{
-		Delay_Cycles(10000000);
-		SRU2(HIGH, DPI_PB14_I);
-		Delay_Cycles(10000000);
-		SRU2(LOW, DPI_PB14_I);
-
-		Delay_Cycles(1000000);
-		DBG(TEST);
-		char temp = buffer_cntr + '0';
-		DBG(&temp);
-		Delay_Cycles(1000000);
-
-		buffer_cntr++;
+		//Delay_Cycles(50000000);
+		//_Run_LED_On();
+		//Delay_Cycles(50000000);
+		//_Run_LED_Off();
 
 		if(flag)
 		{
 			break;
 		}
+
+    	if(inputReady)
+    	{
+    		handleCodecData(buffer_cntr);
+    	}
+
+    	if(commandReady)
+    	{
+			xmitUARTmessage(uart_buffer, 8);
+    		commandReady = 0;
+    	}
 	}
 
 	return 0;
@@ -74,4 +79,15 @@ void Delay_Cycles(unsigned int delayCount)
 {
 	/* delayCount = 1 => 38ns delay */
 	while(delayCount--);
+}
+
+
+void Codec_Reset(void)
+{
+	/* delayCount = 1 => 38ns delay */
+    _Codec_Reset_H();
+    Delay_Cycles(5);
+	_Codec_Reset_L();
+    Delay_Cycles(5);//need¡¶  40ns
+    _Codec_Reset_H();
 }
--- hello-world/system/sru/sru_config.c
+++ hello-world/system/sru/sru_config.c
@@ -1,6 +1,6 @@
 /*
  **
- ** sru_config.c source file generated on 二月 19, 2022 at 15:53:15.
+ ** sru_config.c source file generated on 二月 19, 2022 at 16:15:01.
  **
  ** Copyright (C) 2014-2022 Analog Devices Inc., All Rights Reserved.
  **
