fbd43260a4d8c6cd97dd16cddb60ba08f4c9f932
DelayNoMore <251382196@qq.com>
2022-02-27 07:57:34 +0800

    串口通訊正常,4通道輸入輸入與4通道輸出均正常,使用IN A/B/C/D,OUT 1/2/3/4插槽

--- hello-world/src/initSPORT01_I2S_mode.cpp
+++ hello-world/src/initSPORT01_I2S_mode.cpp
@@ -95,12 +95,12 @@ void initSPORT()
 
     *pCPSP1B = (unsigned int)TCB_RxBlock_B0 - OFFSET + 3;
 
+	// sport0 control register set up as a transmitter in I2S
+	// sport 0 control register, SPCTL0 = 0x000C01F0 */
+	*pSPCTL0 = OPMODE | SPEN_A | SCHEN_A | SDEN_A | SPEN_B | SCHEN_B | SDEN_B | SLEN24 | SPTRAN;
+
 	// sport1 control register set up as a receiver in I2S
 	// sport 1 control register SPCTL1 = 0x000C01F0
 	// externally generated SCLK1 and RFS1
-	*pSPCTL1 = OPMODE | SPEN_A | SCHEN_A | SDEN_A | SLEN24 | SPEN_B | SCHEN_B | SDEN_B;
-
-	// sport0 control register set up as a transmitter in I2S
-	// sport 0 control register, SPCTL0 = 0x000C01F0 */
-	*pSPCTL0 = OPMODE | SPEN_A | SCHEN_A | SDEN_A | SPTRAN | SLEN24 | SPEN_B | SCHEN_B | SDEN_B;
+	*pSPCTL1 = OPMODE | SPEN_A | SCHEN_A | SDEN_A | SPEN_B | SCHEN_B | SDEN_B | SLEN24;
 }
