46138e7f7a008f5ed914e8a23951a2da0b04f2a4
kweiwen <kweiwen.tseng@gmail.com>
2022-03-09 19:36:09 +0800

    init bootloader

---
+++ bootloader/src/bootloader.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * bootloader.h
+ *****************************************************************************/
+
+#ifndef __BOOTLOADER_H__
+#define __BOOTLOADER_H__
+
+/* Add your custom header content here */
+
+#endif /* __BOOTLOADER_H__ */
